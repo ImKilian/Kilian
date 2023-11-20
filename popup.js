6c760169f778387992a82a67eba357efdc51e3ad
ImKilian <144401546+ImKilian@users.noreply.github.com>
2023-11-20 14:19:14 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
 function afficherPopup() {
     let popupBackground = document.querySelector(".popupBackground")
-    
+    // La popup est masquée par défaut (display:none), ajouter la classe "active"
+    // va changer son display et la rendre visible. 
     popupBackground.classList.add("active")
 }
 
 function cacherPopup() {
     let popupBackground = document.querySelector(".popupBackground")
-
     popupBackground.classList.remove("active")
 }
 
@@ -16,17 +16,12 @@ function initAddEventListenerPopup() {
     btnPartage = document.querySelector(".zonePartage button")
     let popupBackground = document.querySelector(".popupBackground")
     btnPartage.addEventListener("click", () => {
-      
         afficherPopup()
     })
 
- 
     popupBackground.addEventListener("click", (event) => {
-     
         if (event.target === popupBackground) {
-        
-            cacherPopup()
+        cacherPopup()
         }
     })
 }
-
